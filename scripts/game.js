10f63ee734864c3ee8834daef5a7a425c8bf6d66
deadr1pley <vanmatrix@hotmail.com>
2025-09-29 18:45:59 +0200

    uppdated spellin

--- scripts/game.js
+++ scripts/game.js
@@ -2,7 +2,7 @@ let game = {
     score: 0,
     currentGame: [],
     playerMoves: [],
-    choices: ["button1", "button2", "button3", "button4"],
+    choices: ["button1", "button2", "button3", "button4"]
 };
 
 
--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-const { test } = require("picomatch");
+// const { test } = require("picomatch");
 const { game } = require("../game");
 
 beforeAll(() => {
@@ -15,7 +15,7 @@ describe("game object contains correct keys", () => {
     test("score key exists", () => {
         expect("score" in game).toBe(true);
     });
-     test("CurrentGame key exists", () => {
+     test("currentGame key exists", () => {
         expect("currentGame" in game).toBe(true);
     });
      test("playerMoves key exists", () => {
@@ -24,7 +24,7 @@ describe("game object contains correct keys", () => {
      test("choices key exists", () => {
         expect("choices" in game).toBe(true);
     });
-    test("choices comtaine correct ids", () => {
+    test("choices contain correct ids", () => {
         expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
     });
 });
