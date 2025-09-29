a46b7d4ddb9ef289410bd6347b2898632985f614
deadr1pley <vanmatrix@hotmail.com>
2025-09-29 19:06:59 +0200

    add code

--- scripts/game.js
+++ scripts/game.js
@@ -3,7 +3,10 @@ let game = {
     currentGame: [],
     playerMoves: [],
     choices: ["button1", "button2", "button3", "button4"]
-};
+}
 
+function newGame() {
+    game.score = 0
+}
 
-module.exports = { game };
+module.exports = { game, newGame };
